3d387ed15b2e8d4ecf55c66cff12694ff3b79b37
BezRafael <rafaelb120934@gmail.com>
2024-08-08 03:46:50 -0300

    adicionando novas rotas (buscar filme e deletar filme)

--- meu_projeto_node/controladores/controle_filmes.js
+++ meu_projeto_node/controladores/controle_filmes.js
@@ -13,9 +13,8 @@ exports.cadastrar_filme = (req, res) => {
     const {id, titulo_original, titulo_br, diretor, ano} = req.body
 
     //verificando se os campos não estão vazios
-    if(!id || !titulo_original ||!titulo_br || !diretor || !ano){
-        res.status(400).send('Preencha Todos os Campos!')
-        return;
+    if(!id || !titulo_original || !titulo_br || !diretor || !ano){
+        res.status(400).send('Preencha Todos os Campos!');
     }
 
     //armazenando o input do usuário em um objeto
@@ -37,4 +36,44 @@ exports.cadastrar_filme = (req, res) => {
 //Função exportada que exibe os filmes cadastrados
 exports.exibir_filmes = (req, res) => {
     res.send(filmes);
+};
+
+
+//Função exportada que busca o filme por ID
+exports.procurar_filme = (req, res) => {
+
+    //armazenando o input do usuário numa variável
+    var input_usuario = req.params.id;
+
+    //varíável que vai realizar uma busca no array e comparar com o input do usuário
+    var busca = filmes.find((filme) => filme.id === input_usuario);
+
+
+    //realizando uma verificação se o usuário adicionou algum parâmetro
+    if (!busca) {
+        res.status(404).send('Filme não Encontrado!');
+    }else{
+        res.status(200).send(busca);
+    }
+};
+
+//Função exportada que deleta o filme por ID
+exports.remover_filme = (req, res) => {
+
+    //armazenando o input do usuário em uma variável
+    var input_usuario = req.params.id;
+
+
+    //varíável que vai realizar uma busca no array e comparar com o input do usuário
+    var busca = filmes.find((filme) => filme.id === input_usuario);
+
+
+    //realizando uma verificação se o usuário adicionou algum parâmetro
+    if (!busca) {
+        res.status(404).send('Filme não Encontrado!');
+    }else{
+        filmes.splice(busca, 1);
+        res.status(200).send('Filme Deletado.');
+    }
+
 };
--- meu_projeto_node/rotas/rota_filmes.js
+++ meu_projeto_node/rotas/rota_filmes.js
@@ -14,6 +14,12 @@ rota.post('/filme', controle_filme.cadastrar_filme);
 //Rota GET (Exibir Filmes)
 rota.get('/filmes', controle_filme.exibir_filmes);
 
+//Rota GET (Procurar Filme)
+rota.get('/filme/:id', controle_filme.procurar_filme);
+
+//Rota DELETE (Deletar Filme)
+rota.delete('/filme/:id', controle_filme.remover_filme);
+
 
 //Exportando a constante
 module.exports = rota;
